8e8a07fa69fa71d5613f861537dbdbd39b64391b
Salvo Camiolo <scc20x@Alpha.cvr.gla.ac.uk>
2019-05-27 11:11:01 +0100

    added the end to end flag to bowtie alignment and the indel calling pipeline

--- snpAnalysis/hcmvSNPAnalysis.py
+++ snpAnalysis/hcmvSNPAnalysis.py
@@ -33,13 +33,25 @@ if vcfFileInput == '0': #Fastq files are provided and alignments/SNP calling nee
 
         print "Analising sample",sampleName
         print "Aigning reads to reference"
-        os.system("bowtie2 -1 "+read1+" -2 "+read2+" -x reference -S alignment.sam -p "+numThreads)
+        os.system("bowtie2 --end-to-end -1 "+read1+" -2 "+read2+" -x reference -S alignment.sam -p "+numThreads)
         print "Converting sam to bam"
         os.system("samtools view -bS -h alignment.sam >alignment.bam")
         print "Sorting bam"
         os.system("samtools sort -o "+sampleName+"_alignment_sorted.bam alignment.bam")
         print "Calling snps with lofreq"
+        #Analyze snps with lowfreq
         os.system("lofreq call -f "+referenceFile+" -o "+sampleName+".vcf "+sampleName+"_alignment_sorted.bam")
+        
+        #Analyze indes using the GATK pipeline
+        os.system("samtools view -bF 4 "+sampleName+"_alignment_sorted.bam >mapped.bam")
+        os.system("~/Software/jre1.8.0_191/bin/java -jar -XX:ParallelGCThreads=16  ~/Software/picard.jar AddOrReplaceReadGroups I=mapped.bam O=rg_added_sorted.bam SO=coordinate RGID=id RGLB=library RGPL=Ilumina RGPU=machine RGSM=Consensus")
+        os.system("~/Software/jre1.8.0_191/bin/java -jar -XX:ParallelGCThreads=16  ~/Software/picard.jar MarkDuplicates I=rg_added_sorted.bam O=dedupped.bam  CREATE_INDEX=true VALIDATION_STRINGENCY=SILENT M=output.metrics")
+        os.system("~/Software/jre1.8.0_191/bin/java -jar ~/Software/picard.jar CreateSequenceDictionary R="+referenceFile)
+        os.system("samtools faidx "+referenceFile)
+        os.system("~/Software/jre1.8.0_191/bin/java -jar  ~/Software/gatk3.8/GenomeAnalysisTK.jar -T  HaplotypeCaller -R "+referenceFile+" -I dedupped.bam  -o output.vcf -A StrandAlleleCountsBySample")
+        os.system("mv output.vcf "+sampleName+"_indels.vcf")
+               
+        
         os.system("mv "+sampleName+"_alignment_sorted.bam "+outputFolder+"/")
         os.system("mv *.vcf "+outputFolder+"/")
         os.system("rm alignment* -f")
