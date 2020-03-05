fc765a1c1826d92f6cca200ee8800b40742ef1f2
Salvo Camiolo <scc20x@Alpha.cvr.gla.ac.uk>
2020-03-05 17:47:15 +0000

    improved installation6

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ pip install biopython --user
 pip install Pillow --user
 pip install reportlab --user
 pip install matplotlib --user
-pip3 install --user --upgrade cutadapt
+pip install --user --upgrade cutadapt
 
 
