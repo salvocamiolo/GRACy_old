e66b68c96b8abff1b0d6f30ccc33230087dfd2f6
Salvo Camiolo <scc20x@Alpha.cvr.gla.ac.uk>
2020-03-05 17:06:47 +0000

    installation procedure improved

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@ pip install khmer --user
 pip install biopython --user
 pip install Pillow --user
 pip install reportlab --user
+pip install matplotlib --user
+pip install --user --upgrade cutadapt
 
 
